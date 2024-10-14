4c397c6a6e7ee8e191b627d7a9ff1fe0bb12054e
mooleshacat <43627985+mooleshacat@users.noreply.github.com>
2024-10-13 20:07:45 -0700

    added redis patch manually - patcher not working as code changed

--- src/invidious.cr
+++ src/invidious.cr
@@ -63,12 +63,13 @@ alias IV = Invidious
 CONFIG   = Config.load
 HMAC_KEY = CONFIG.hmac_key
 
-PG_DB    = DB.open CONFIG.database_url
+PG_DB       = DB.open CONFIG.database_url
 REDIS_DB = Redis::PooledClient.new(unixsocket: CONFIG.redis_socket || nil, url: CONFIG.redis_url || nil)
 
 if REDIS_DB.ping
   puts "Connected to redis"
 end
+
 ARCHIVE_URL = URI.parse("https://archive.org")
 PUBSUB_URL  = URI.parse("https://pubsubhubbub.appspot.com")
 REDDIT_URL  = URI.parse("https://www.reddit.com")
