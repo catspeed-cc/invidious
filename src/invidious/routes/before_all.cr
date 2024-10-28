e6356c46bb5089630377300f90043482b125d711
mooleshacat <43627985+mooleshacat@users.noreply.github.com>
2024-10-28 18:47:47 -0400

    another test

--- src/invidious/routes/before_all.cr
+++ src/invidious/routes/before_all.cr
@@ -49,7 +49,7 @@ module Invidious::Routes::BeforeAll
       domain2 = schema + "*." + theDomain    
       LOGGER.debug("DOMAIN 1: " + domain1)
       LOGGER.debug("DOMAIN 2:" + domain2)      
-      permitted = (domain1 + " " + domain2)
+      permitted = "" + domain1 + " " + domain2 + ""
 
     else      
       permitted = "'self'"
