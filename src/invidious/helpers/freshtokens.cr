23c334a415ae46fe034fbff94acba75c44218668
mooleshacat <43627985+mooleshacat@users.noreply.github.com>
2024-10-29 09:07:28 -0400

    fix pot?

--- src/invidious/helpers/freshtokens.cr
+++ src/invidious/helpers/freshtokens.cr
@@ -24,7 +24,9 @@ class Invidious::FreshTokens
     # get the tokens :)
     tokendata = `${HOME}/.nvm/versions/node/v20.18.0/bin/node submodules/youtube-po-token-generator/examples/one-shot.js`
     
+    LOGGER.info("TOKENDATA1: #{tokendata}")
     @@pot = `echo "#{tokendata.strip}" | awk -F"'" '/poToken/{print $4}'`
+    LOGGER.info("TOKENDATA2: #{tokendata}")
     @@vdata = `echo "#{tokendata.strip}" | awk -F"'" '/visitorData/{print $2}'`
 
   end
