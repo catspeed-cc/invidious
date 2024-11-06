280c4718ff90fefe4f0531a3e6ee469fcecaa370
mooleshacat <43627985+mooleshacat@users.noreply.github.com>
2024-11-05 23:51:20 -0500

    updated log lines and such

--- src/invidious/jobs/freshtokens.cr
+++ src/invidious/jobs/freshtokens.cr
@@ -5,12 +5,12 @@ class Invidious::Jobs::FreshTokensUserJob < Invidious::Jobs::BaseJob
     loop do
       
       # job start
-      LOGGER.info("jobs: running FreshTokensUser job")
+      LOGGER.info("FreshTokens: jobs: running FreshTokensUser job")
       
       # do user jobs
 
       # job done
-      LOGGER.info("jobs: FreshTokensUser done.")
+      LOGGER.info("FreshTokens: jobs: FreshTokensUser done.")
 
       # static sleep is all that is needed for now
       sleep 1000.seconds
@@ -26,13 +26,13 @@ class Invidious::Jobs::FreshTokensAnonJob < Invidious::Jobs::BaseJob
     loop do
       
       # job start
-      LOGGER.info("jobs: running FreshTokensAnon job")
+      LOGGER.info("FreshTokens: jobs: running FreshTokensAnon job")
       
       # do anon jobs
       FreshTokens.generate_anon_tokens
 
       # job done
-      LOGGER.info("jobs: FreshTokensAnon done.")
+      LOGGER.info("FreshTokens: jobs: FreshTokensAnon done.")
       
       # static sleep is all that is needed for now
       sleep 30.seconds
@@ -48,12 +48,14 @@ class Invidious::Jobs::FreshTokensStatsJob < Invidious::Jobs::BaseJob
     loop do
       
       # job start
-      LOGGER.info("jobs: running FreshTokensStats job")
+      LOGGER.info("FreshTokens: jobs: running FreshTokensStats job")
+      
+      LOGGER.info("FreshTokens: TEST: \"#{REDIS_CLI}\"")
       
       FreshTokens.update_stats
 
       # job done
-      LOGGER.info("jobs: FreshTokensStats done.")
+      LOGGER.info("FreshTokens: jobs: FreshTokensStats done.")
       
     end
   end
